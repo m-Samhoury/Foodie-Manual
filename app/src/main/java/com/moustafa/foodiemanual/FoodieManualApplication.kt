b816fb6bbf7aa2935f5ff66a83a09228e22f5882
MrSamhoury <moustafa.samhoury@gmail.com>
2019-11-04 22:02:16 +0200

    Initial commit

---
+++ app/src/main/java/com/moustafa/foodiemanual/FoodieManualApplication.kt
@@ -0,0 +1,15 @@
+package com.moustafa.foodiemanual
+
+import android.app.Application
+
+/**
+ * @author moustafasamhoury
+ * created on Monday, 04 Nov, 2019
+ */
+
+class FoodieManualApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
