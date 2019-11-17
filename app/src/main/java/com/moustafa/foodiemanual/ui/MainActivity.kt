eba71ac25fccde0133604911302a23355233ad5a
MrSamhoury <moustafa.samhoury@gmail.com>
2019-11-17 14:08:58 +0200

    set up navigation component

---
+++ app/src/main/java/com/moustafa/foodiemanual/ui/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.moustafa.foodiemanual.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.moustafa.foodiemanual.R
+
+/**
+ * @author moustafasamhoury
+ * created on Sunday, 17 Nov, 2019
+ */
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private val appBarConfiguration = AppBarConfiguration(emptySet())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupViews()
+    }
+
+    private fun setupViews() {
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+
+        setupActionBarWithNavController(
+            navController = host.navController,
+            configuration = appBarConfiguration
+        )
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
+    }
+
+}
